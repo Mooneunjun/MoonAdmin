ae116ab0a3cb7129fb94ba885bd6dcdef670dbe4
Mooneunjun <mooneunjun@icloud.com>
2023-08-28 01:24:56 +0900

    login fail message update

--- login.js
+++ login.js
@@ -9,9 +9,14 @@ async function fetchData(userId) {
         id: userId,
       }),
     });
+
+    if (response.status === 404) {
+      console.log("User not found");
+      // 로그인 실패 메시지를 화면에 표시하는 로직 추가
+      return null;
+    }
+
     const data = await response.json();
-    // console.log("API Response:", response);
-    // console.log("API Data:", data);
     return data;
   } catch (error) {
     console.error("Error:", error);
@@ -66,7 +71,7 @@ if (localStorage.getItem("UserLogin")) {
     document.querySelector(".login-box").classList.add("hidden");
     document.querySelector(".bg-shadow").classList.add("hidden");
   }, 1000);
-  logoName.innerText = UserLogin.Name;
+  logoName.innerText = UserLogin.name;
 } else {
   document.querySelector(".login-box").classList.remove("hidden");
   document.querySelector(".bg-shadow").classList.remove("hidden");
@@ -80,17 +85,22 @@ logout.addEventListener("click", () => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  var form = document.querySelector("#login-form"); // ID를 사용해 form 요소를 찾습니다.
+  var form = document.querySelector("#login-form");
+  var submitSec = document.querySelector(".submit-sec p");
+  var logoName = document.querySelector(".logo-name span");
 
   form.addEventListener("submit", async function (event) {
     event.preventDefault();
 
-    var username = document.querySelector(".username-input").value;
+    var userId = document.querySelector(".userid-input").value;
     var password = document.querySelector(".password-input").value;
-    var submitSec = document.querySelector(".submit-sec p");
-    var logoName = document.querySelector(".logo-name span");
 
-    const data = await fetchData(username);
+    const data = await fetchData(userId);
+
+    if (!data) {
+      submitSec.innerText = "아이디가 존재하지 않습니다.";
+      return;
+    }
 
     if (data.password === password) {
       console.log("로그인 성공");
@@ -101,7 +111,7 @@ document.addEventListener("DOMContentLoaded", function () {
       localStorage.setItem("UserLoginToken", JSON.stringify(data.pageId));
     } else {
       console.log("로그인 실패");
-      submitSec.innerText = "아이디 또는 비밀번호가 일치하지 않습니다.";
+      submitSec.innerText = "비밀번호가 일치하지 않습니다.";
     }
   });
 });
