3e309f90b27715a61ca6488f5aeb86c785de39d9
Mooneunjun <mooneunjun@icloud.com>
2023-09-02 00:21:55 +0900

    users page test update

--- settings.js
+++ settings.js
@@ -1,14 +1,25 @@
 const NavDashboard = document.querySelector(".nav-dashboard");
 const NavSettings = document.querySelector(".nav-settings");
+const NavUsers = document.querySelector(".nav-users");
+const BxGroup = document.querySelector(".bx-group");
 const BxsDashboard = document.querySelector(".bxs-dashboard");
 const BxCog = document.querySelector(".bx-cog");
 
 BxCog.addEventListener("click", () => {
   NavDashboard.classList.add("hidden");
+  NavUsers.classList.add("hidden");
   NavSettings.classList.remove("hidden");
 });
 
 BxsDashboard.addEventListener("click", () => {
-  NavDashboard.classList.remove("hidden");
+  NavUsers.classList.add("hidden");
+
   NavSettings.classList.add("hidden");
+  NavDashboard.classList.remove("hidden");
+});
+
+BxGroup.addEventListener("click", () => {
+  NavDashboard.classList.add("hidden");
+  NavSettings.classList.add("hidden");
+  NavUsers.classList.remove("hidden");
 });
